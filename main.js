68aa886da2bdc5cffddead760f15515f582dec2a
KeoKimHeng <142481630+KeoKimHeng@users.noreply.github.com>
2024-03-03 15:33:23 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 onload = () =>{
         document.body.classList.remove("container");
 };
+document.querySelector('.play-audio').addEventListener('click', function() {
+        var audio = document.getElementById('myAudio');
+        audio.play();
+    });
